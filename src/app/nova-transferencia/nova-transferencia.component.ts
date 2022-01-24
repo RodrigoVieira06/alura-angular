c8a5eddbab2cee9c65e5196b8d2e32afe66a1bf1
RodrigoVieira06 <rodrigovlima06@gmail.com>
2022-01-24 13:11:18 -0300

    Curso Angular Alura - Aula 2 - Formulário de nova transferência

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,10 +1,18 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
-    selector:'app-nova-transferencia',
-    templateUrl:'./nova-transferencia.component.html',
-    styleUrls:['nova-transferencia.component.scss']
+  selector: 'app-nova-transferencia',
+  templateUrl: './nova-transferencia.component.html',
+  styleUrls: ['nova-transferencia.component.scss'],
 })
 export class NovaTransferenciaComponent {
 
+  valor: number = 12;
+  destino: number = 222;
+
+  transferir(): void {
+    console.log('Solicitada nova transferência');
+    console.log("Valor: ", this.valor);
+    console.log("Destino: ", this.destino);
+  }
 }
